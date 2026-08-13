9993505de4d48e29c5be7d911231dd213266bae0
Your Name <apoorvajaiswal05@gmail.com>
2026-08-13 14:24:26 +0530

    Added Amazon shopping cart project

--- index2.js
+++ index2.js
@@ -1,5 +1,5 @@
 import fs from "fs/promises";
-import { writeFile, readFile } from "fs/promises";
+import { writeFile, readFile, appendFile } from "fs/promises";
 //await writeFile("stud.txt","Ravikant Singh\n RollNo. 84");
     //console.log("File written")
 //const data = readFile("stud.txt","utf-8");
@@ -13,7 +13,11 @@ const readContent = async (fname) => {
     const data = await readFile(fname, "utf-8");
     return data;
 };
-
+const appendData = async (fname,content) =>{
+    await appendFile (fname,"\n"+ content);
+    console.log("data appended")
+};
 await addContent("notes.txt", "FS is easy in JS");
 console.log(await readContent("notes.txt"));
-
+await  appendData('notes.txt', "it can add, read and update content")
+console.log("Updated Contents\n", await  readContent("notes.txt")); 
---
+++ readme.md
@@ -0,0 +1,13 @@
+CRUD: Create Read Update Delete 
+# File System of NodeJS 
+It allows JS code running outside the browser and interact directly to the operating system
+
+## Common operaions in File/folder 
+- Reading and writing file-> readFile(), writeFile() , appendFile()
+-Directory management-> mkdir(), rmdir(), readdir()
+-Metadata/information-> stat(), lstat(), fstat()
+-Watching for changes-> watch(), watchFile(), unwatchFile()
+-Streaming Large Files-> createReadStream(), createwriteStream()
+-File Operations-> rename(), truncate(), unlink(), link(), syslink()
+
+await can be used with any promise in async function. That async function will also accessed by await keyword
