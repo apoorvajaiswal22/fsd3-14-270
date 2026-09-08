c1941a9298caf0af0de0aa32faf5b13639adf75f
Your Name <apoorvajaiswal05@gmail.com>
2026-09-08 10:07:24 +0530

    .

--- LAB 3/app.js
+++ LAB 3/app.js
@@ -1,9 +1,69 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {
-    res.end("<h1>Welcome to Internal SIH</h1>");
+import { getAllTeams } from './teams.js';
+
+const PORT = 5000;
+
+const sendJSON = (res, data) => {
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+        data === undefined
+            ? JSON.stringify({ message: "Not Found" })
+            : JSON.stringify(data)
+    );
+};
+
+
+const parseJSONBody = (req) => {
+
+    return new Promise((resolve, reject) => {
+
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+
+            try {
+                const jsonData = JSON.parse(body);
+                resolve(jsonData);
+            } catch (error) {
+                reject(error);
+            }
+
+        });
+
+        req.on('error', error => {
+            reject(error);
+        });
+
+    });
+};
+
+
+const server = http.createServer(async (req, res) => {
+
+    if (req.url === '/teams' && req.method === 'GET') {
+
+        const allTeams = getAllTeams();
+
+        sendJSON(res, allTeams);
+
+    } else {
+
+        res.statusCode = 404;
+
+        sendJSON(res);
+
+    }
+
 });
 
-server.listen(5000, () => {
-    console.log("Server is running");
+
+server.listen(PORT, () => {
+    console.log("Server is running at", PORT);
 });
